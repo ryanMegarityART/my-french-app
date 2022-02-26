abf9f5b8e42e7338e38b56cd7dcc335d66358ba8
ryanMegarityART <ryan.megarity@pshealth.co.uk>
2022-02-26 16:55:48 +0000

    Added better response styling

--- src/CompleteTheSentence/CompleteTheSentence.tsx
+++ src/CompleteTheSentence/CompleteTheSentence.tsx
@@ -1,25 +1,34 @@
 import React, { useCallback, useEffect, useState } from "react";
 import { Button, Card, Container } from "react-bootstrap";
-import { Success } from "../Shared/Success";
-import { Error } from "../Shared/Error";
+// import { Success } from "../Shared/Success";
+// import { Error } from "../Shared/Error";
 import { Sentence, sentences } from "./sentences";
 
 export const CompleteTheSentence = () => {
   const [selectedSentence, setSelectedSentence] = useState<Sentence>();
   const [selectedSentenceWithGapFormat, setSelectedSentenceWithGapFormat] =
     useState<string>("");
-  const [showSuccess, setShowSucces] = useState(false);
-  const [showError, setShowError] = useState(false);
-  useEffect(() => {
-    const sentence = sentences[0];
-    sentence.options.sort((a, b) => {
-      if (Math.random() > 0.5) {
-        return 1;
-      }
-      return -1;
-    });
+  const [answerCorrect, setAnswerCorrect] = useState(false);
+  const [showResponse, setShowResponse] = useState(false);
+  const [selectedOption, setSelectedOption] = useState<any>();
+  const [score, setScore] = useState(0);
+
+  const selectNewSentence = useCallback(() => {
+    const randomIndex = Math.floor(Math.random() * sentences.length);
+    const sentence = sentences[randomIndex];
+    // sentence.options.sort((a, b) => {
+    //   if (Math.random() > 0.5) {
+    //     return 1;
+    //   }
+    //   return -1;
+    // });
     setSelectedSentence(sentence);
-  }, []);
+  }, [setSelectedSentence]);
+
+  useEffect(() => {
+    selectNewSentence();
+  }, [selectNewSentence]);
+
   useEffect(() => {
     setSelectedSentenceWithGapFormat(
       selectedSentence
@@ -31,41 +40,84 @@ export const CompleteTheSentence = () => {
         : ""
     );
   }, [selectedSentence]);
-  const handleClick = useCallback((option) => {
-    console.log(option);
-    option.correct ? setShowSucces(true) : setShowError(true);
-    setTimeout(() => {
-      setShowError(false);
-      setShowSucces(false);
-    }, 1500);
-  }, []);
+
+  const handleClick = useCallback(
+    (option) => {
+      console.log(option);
+      option.correct ? setScore(score + 1) : setScore(0);
+      console.log(score);
+      setSelectedOption(option);
+      setAnswerCorrect(option.correct);
+      setShowResponse(true);
+      setTimeout(() => {
+        setAnswerCorrect(false);
+        setShowResponse(false);
+        selectNewSentence();
+      }, 800);
+    },
+    [score, selectNewSentence]
+  );
+
+  const returnButtonVariant = useCallback(
+    (option: any) => {
+      if (showResponse) {
+        if (option.id === selectedOption?.id && option.correct) {
+          return "success";
+        } else if (option.id === selectedOption?.id) {
+          return "danger";
+        } else {
+          return "secondary";
+        }
+      }
+      return "secondary";
+    },
+    [selectedOption, showResponse]
+  );
+
   return (
     <div>
-      {showSuccess && <Success />}
-      {showError && <Error />}
-      {!showSuccess && !showError && (
-        <Card className="text-center" bg="dark" style={{ zIndex: 100 }}>
-          <Card.Header>compléter la phrase</Card.Header>
-          <Card.Body>
-            <Card.Text>{selectedSentenceWithGapFormat}</Card.Text>
-            <Container className="multipleChoice">
-              {selectedSentence &&
-                selectedSentence.options.map((option) => {
-                  return (
-                    <Button
-                      variant="secondary"
-                      className="m-1"
-                      key={option.value}
-                      onClick={() => handleClick(option)}
-                    >
-                      {option.value}
-                    </Button>
-                  );
-                })}
-            </Container>
-          </Card.Body>
-        </Card>
-      )}
+      <Card
+        className="text-center"
+        bg="dark"
+        style={{ zIndex: 100, minWidth: "50vw" }}
+      >
+        <Card.Header className="mt-3">
+          <h2>compléter la phrase</h2>
+        </Card.Header>
+        <Card.Body>
+          <Card.Text>{selectedSentenceWithGapFormat}</Card.Text>
+          <Container className="multipleChoice">
+            {selectedSentence &&
+              selectedSentence.options.map((option) => {
+                return (
+                  <Button
+                    variant={returnButtonVariant(option)}
+                    className="m-1"
+                    key={option.id}
+                    onClick={() => handleClick(option)}
+                  >
+                    {option.value}
+                  </Button>
+                );
+              })}
+          </Container>
+        </Card.Body>
+      </Card>
+
+      <Card
+        className="text-center"
+        bg="dark"
+        style={{ zIndex: 100, minWidth: "25vw", margin: "15px" }}
+      >
+        <Card.Header className="mt-3">
+          <h4>série victorieuse</h4>
+        </Card.Header>
+        <Card.Body>
+          <Card.Text>
+            <h4>{score}</h4>
+          </Card.Text>
+        </Card.Body>
+      </Card>
     </div>
   );
 };
--- src/CompleteTheSentence/sentences.ts
+++ src/CompleteTheSentence/sentences.ts
@@ -1,6 +1,7 @@
 export interface Sentence {
   sentenceWithGap: string;
   options: {
+    id: number;
     value: string;
     correct: boolean;
   }[];
@@ -10,9 +11,17 @@ export const sentences: Sentence[] = [
   {
     sentenceWithGap: "Je |DELIMITER| un cafe s'il vous plait.",
     options: [
-      { value: "voudrais", correct: true },
-      { value: "voir", correct: false },
-      { value: "fait", correct: false },
+      { id: 1, value: "voudrais", correct: true },
+      { id: 2, value: "voir", correct: false },
+      { id: 3, value: "fait", correct: false },
+    ],
+  },
+  {
+    sentenceWithGap: "|DELIMITER| mon frère",
+    options: [
+      { id: 1, value: "C'est", correct: true },
+      { id: 2, value: "Il est", correct: false },
+      { id: 3, value: "Elle est", correct: false },
     ],
   },
 ];
